1155a451351a9245f7a1727f4d77cd28e9298b03
Maria Jose <147551384+Majou07@users.noreply.github.com>
2025-12-29 01:25:24 -0600

    cambios majo

---
+++ app/src/main/java/uv/tc/packetworld/util/Constantes.kt
@@ -0,0 +1,6 @@
+package uv.tc.packetworld.util
+
+class Constantes {
+
+    val URL_API = "http://localhost:8080/APIRestPW/"
+}
---
+++ app/src/main/java/uv/tc/packetworld/util/LoginActivity.kt
@@ -0,0 +1,41 @@
+package uv.tc.packetworld.util
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle){
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnIngresar.setOnClickListener {
+            verificarCrenciales()
+        }
+    }
+
+    fun verificarCrenciales(){
+        if (sonCamposValidos()){
+            consumirAPI(binding.etMatricula.text.toString(),
+                binding.etPassword.text.toString())
+        }
+    }
+
+    fun sonCamposValidos(): Boolean{
+        var valido = true
+        if (binding.etMatricula.text.isEmpty()){
+            binding.etMatricula.setError("Matricula obligatoria")
+            valido = false
+        }
+
+        if (binding.etPassword.text.isEmpty()){
+            binding.etPassword.setError("Contraseña obligatoria")
+            valido = false
+        }
+        return valido;
+    }
+}
