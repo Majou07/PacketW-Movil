3599042de82b4f14337375843ddf538577800448
Maria Jose <147551384+Majou07@users.noreply.github.com>
2026-01-05 03:14:29 -0600

    Envio pojo

--- app/src/main/java/uv/tc/packetworld/pojo/Colaborador.kt
+++ app/src/main/java/uv/tc/packetworld/pojo/Colaborador.kt
@@ -1,3 +1,10 @@
 package uv.tc.packetworld.pojo
 
-data class Colaborador()
+data class Colaborador(
+    val idColaborador: Int,
+    val nombre: String,
+    val apellidoPaterno: String,
+    val apellidoMaterno: String?,
+    val numeroPersonal: String,
+    val correoElectronico: String
+)
